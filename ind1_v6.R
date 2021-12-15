0a03d6a21ed04603b65c9a79e6c15e45171e9c64
GuidoSaborio <60102773+GuidoSaborio@users.noreply.github.com>
2021-12-15 14:31:08 -0600

    Update ind1_v6.R

--- ind1_v6.R
+++ ind1_v6.R
@@ -30,7 +30,7 @@ RNVSPHPM
 #libreria openxlsx
 "Para el indicador 1, todos los datos se encuentran en la hoja Formulario Indicador 1 "
 
-df<-read.xlsx("C:/Users/Isabel/Desktop/PPS/Proyecto PRONAMEC/Bases de datos/PARQUE NACIONAL SANTA ROSA.xlsm", 
+df<-read.xlsx("Bases de datos/PARQUE NACIONAL SANTA ROSA.xlsm", 
               sheet = "Formulario Indicador 1")
 View(df)
 
